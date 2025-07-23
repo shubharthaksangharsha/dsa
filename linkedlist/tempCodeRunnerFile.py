992f2be8be1a6a26d5844054e2433548b8fecf28
shubharthaksangharsha <shubharthaksangharsha@gmail.com>
2025-07-23 12:14:05 +0530

    Removed files listed in .gitignore from Git tracking

--- linkedlist/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-(head: Node) -> Node: 
-#     #base
-#     if head is None or head.next is None: 
-#         return head 
-    
-#     small_head = reverse_recursive(head.next) 
-#     head.next.next = head 
-#     head.next = None 
-#     return small_head
